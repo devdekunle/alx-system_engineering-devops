8474a0b1a56211cc6edb953bfe467819b18ec707
Abraham Samuel Adekunle <abrahamadekunle50@gmail.com>
2023-04-19 22:44:47 +0100

    create holberton_user and assign permissions

--- 0x14-mysql/create_user.sql
+++ 0x14-mysql/create_user.sql
@@ -1,4 +1,4 @@
 -- create a user holberton_user
 CREATE USER IF NOT EXISTS 'holberton_user'@'localhost' IDENTIFIED BY 'projectcorrection280hbtn';
-GRANT ALL PRIVILEGES ON *.* TO 'holberton'@'localhost';
+GRANT ALL PRIVILEGES ON *.* TO 'holberton_user'@'localhost';
 FLUSH PRIVILEGES;
