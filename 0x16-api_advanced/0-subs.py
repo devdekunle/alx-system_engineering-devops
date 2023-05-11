61330ac68203ec9688efd924b1964caf300b517d
Abraham Samuel Adekunle <abrahamadekunle50@gmail.com>
2023-05-11 11:47:20 +0100

    get number of subscribers for a sunreddit

---
+++ 0x16-api_advanced/0-subs.py
@@ -0,0 +1,20 @@
+#!/usr/bin/python3
+"""
+queries the Reddit API and returns the number
+of subscribers (not active users, total subscribers)
+for a given subreddit.
+"""
+import requests
+
+
+def number_of_subscribers(subreddit):
+    """ returns the number of subscribers for a subreddit"""
+    subreddit = requests.get('https://www.reddit.com/r/{}/about.json'
+                            .format(subreddit),
+                            headers={'user-agent': "google-chrome"},
+                            allow_redirects=False)
+
+    if subreddit.status_code >=300:
+        return 0
+
+    return subreddit.json().get('data').get('subscribers')
