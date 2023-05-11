eb333a5ee96910c34c87a2b8a6f6ca0b4d842443
Abraham Samuel Adekunle <abrahamadekunle50@gmail.com>
2023-05-11 15:05:11 +0100

    print title of hot posts for a subreddit from reddit api

---
+++ 0x16-api_advanced/1-top_ten.py
@@ -0,0 +1,18 @@
+#!/usr/bin/python3
+"""
+prints the first 10 posts of the hotlist
+"""
+import requests
+
+
+def top_ten(subreddit):
+    """ prints the titles"""
+    sub_reddit = requests.get("https://www.reddit.com/r/{}/hot.json?limit=10"
+                              .format(subreddit),
+                               headers={"user-agent":"google-chrome"},
+                              allow_redirects=False)
+    if sub_reddit.status_code >= 300:
+        print('None')
+
+    for value in sub_reddit.json().get('data').get('children'):
+        print(value.get('data').get('title'))
